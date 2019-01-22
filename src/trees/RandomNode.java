a388a9c610558121911c952c56dd2a33d532e579
irolea <kta_90_aug@yahoo.com>
2019-01-22 20:57:08 +0200

    Trees

---
+++ src/trees/RandomNode.java
@@ -0,0 +1,14 @@
+package trees;
+
+import org.junit.Test;
+
+/**
+ * https://cs.stackexchange.com/questions/83540/how-to-select-a-binary-tree-node-uniformly-at-random
+ */
+public class RandomNode {
+
+    @Test
+    public void randomNode()    {
+        System.out.println("Work in progress...random node");
+    }
+}
