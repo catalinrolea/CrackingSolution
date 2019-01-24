634917c03bab535d75c4253b3280b16c24e3842b
irolea <kta_90_aug@yahoo.com>
2019-01-25 00:40:36 +0200

    Trees

--- src/trees/RandomNode.java
+++ src/trees/RandomNode.java
@@ -7,8 +7,78 @@ import org.junit.Test;
  */
 public class RandomNode {
 
+    class TreeNode {
+        private int key;
+        private TreeNode left, right, parent;
+
+        TreeNode(int key) {
+            this.key = key;
+            this.left = this.right = this.parent = null;
+        }
+    }
+
+    TreeNode root;
+
+    public RandomNode() {
+        root = null;
+    }
+
+    void inorder(TreeNode node) {
+        if (node == null)
+            return;
+
+        inorder(node.left);
+        System.out.print(node.key + " ");
+        inorder(node.right);
+    }
+
+    public void insertStandard(int key) {
+        root = insertRecursivelyBinaryTree(root, key);
+    }
+
+    TreeNode insertRecursivelyBinaryTree(TreeNode node, int key) {
+        if (node == null) {
+            node = new TreeNode(key);
+            return node;
+        }
+        TreeNode temporaryNode = null;
+        if (key < node.key) {
+            temporaryNode = insertRecursivelyBinaryTree(node.left,key);
+             node.left = temporaryNode;
+             temporaryNode.parent = node;
+        }
+        if (key > node.key) {
+            temporaryNode = insertRecursivelyBinaryTree(node.right,key);
+            node.right = temporaryNode;
+            temporaryNode.parent = node;
+        }
+        return node;
+    }
+
+    TreeNode findNode(TreeNode node, int key) {
+
+        if (node == null)
+            return null;
+
+
+
+        return node;
+    }
+
     @Test
-    public void randomNode()    {
-        System.out.println("Work in progress...random node");
+    public void randomNode() {
+        System.out.println("Work in progress...get random node from a binary tree");
+        RandomNode treeNode = new RandomNode();
+        treeNode.insertStandard(20);
+        treeNode.insertStandard(10);
+        treeNode.insertStandard(30);
+        treeNode.insertStandard(15);
+        treeNode.insertStandard(17);
+        treeNode.insertStandard(5);
+        treeNode.insertStandard(3);
+        treeNode.insertStandard(7);
+      //  treeNode.inorder(treeNode.root);
+
+        System.out.println(treeNode.findNode(treeNode.root,10));
     }
 }
